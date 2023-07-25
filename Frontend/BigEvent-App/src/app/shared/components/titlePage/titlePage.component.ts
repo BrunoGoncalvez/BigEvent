52ff0fd9763ccf6e5d2c414ba8fb0c172a937249
BrunoGoncalvez <brunosgoncalvez@gmail.com>
2023-07-24 22:51:36 -0300

    BEV-0007 - Criação de novos componentes

---
+++ Frontend/BigEvent-App/src/app/shared/components/titlePage/titlePage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-page',
+  templateUrl: './titlePage.component.html',
+  styleUrls: ['./titlePage.component.scss']
+})
+export class TitlePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
