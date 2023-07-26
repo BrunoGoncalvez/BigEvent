592b3c53a55c02cb4080b020f8d9e8aedf849f39
BrunoGoncalvez <brunosgoncalvez@gmail.com>
2023-07-25 22:13:06 -0300

    BEV-0008 - Definição das Rotas e Criação de novos componentes

--- Frontend/BigEvent-App/src/app/app-routing.module.ts
+++ Frontend/BigEvent-App/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventsComponent } from './components/events/events.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'speakers', component: SpeakersComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: NotFoundPageComponent },
+  // { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
