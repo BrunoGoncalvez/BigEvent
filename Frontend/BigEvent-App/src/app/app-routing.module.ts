b3c58bb71b69d86903858d22853b14a9afad2bb1
BrunoGoncalvez <brunosgoncalvez@gmail.com>
2023-07-27 23:28:13 -0300

    BEV-0011 - Criação de Sub Components e Sub Rotas para Eventos

--- Frontend/BigEvent-App/src/app/app-routing.module.ts
+++ Frontend/BigEvent-App/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailEventComponent } from './components/events/detail-event/detail-event.component';
 import { EventsComponent } from './components/events/events.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListEventsComponent } from './components/events/list-events/list-events.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SpeakersComponent } from './components/speakers/speakers.component';
@@ -12,7 +14,11 @@ import { SpeakersComponent } from './components/speakers/speakers.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'events', component: EventsComponent },
+  { path: 'events', component: EventsComponent, children: [
+    { path: 'list', component: ListEventsComponent },
+    { path: 'details', component: DetailEventComponent },
+    { path: 'details/:id', component: DetailEventComponent }
+  ] },
   { path: 'speakers', component: SpeakersComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'contacts', component: ContactsComponent },
