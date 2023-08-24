a2a6116ba30d2c6dd150bcf692572a30c5edcea2
Bruno Gonçalves <brunosgoncalvez@gmail.com>
2023-08-24 13:05:24 -0300

    BEV-0018 Criação de Mappings e Classes DTO

---
+++ Backend/src/BigEvent.Application/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BigEvent.Application.ModelsDTO;
+using BigEvent.Core.Models;
+
+namespace BigEvent.Application.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+
+        public AutoMapperProfile()
+        {
+
+            CreateMap<Allotment, AllotmentDTO>().ReverseMap();
+            CreateMap<Event, EventDTO>().ReverseMap();
+            CreateMap<SocialMedia, SocialMediaDTO>().ReverseMap();
+            CreateMap<Speaker, SpeakerDTO>().ReverseMap();
+
+        }
+
+    }
+}
---
+++ Backend/src/BigEvent.Application/ModelsDTO/EventDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+
+namespace BigEvent.Application.ModelsDTO
+{
+    public class EventDTO
+    {
+
+        public int Id { get; set; }
+
+        public string Local { get; set; }
+
+        public string EventDate { get; set; }
+
+        public string Theme { get; set; }
+
+        public int MaximumGuests { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+
+        public IEnumerable<AllotmentDTO> Allotments { get; set; }
+
+        public IEnumerable<SocialMediaDTO> SocialMedias { get; set; }
+
+        public IEnumerable<SpeakerDTO> Speakers { get; set; }
+
+    }
+}
